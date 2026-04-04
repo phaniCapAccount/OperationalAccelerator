8be7e8315c86d916956d063e69e8151607455690
Kondiboena <kondiboena.phaneendra@capgemini.com>
2026-04-04 16:54:12 -0400

    Initial commit: Terraform Azure app infrastructure

---
+++ main.tf
@@ -0,0 +1,41 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  os_type             = "Linux"
+  sku_name            = var.sku_size
+}
+
+resource "azurerm_linux_web_app" "example" {
+  name                = var.web_app_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {}
+}
+
+# Create or use existing Virtual Network
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  tags = {
+    environment = "Terraform"
+  }
+}
+
+# Create or use existing Subnet
+resource "azurerm_subnet" "example" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = [var.subnet_address_prefix]
+}
